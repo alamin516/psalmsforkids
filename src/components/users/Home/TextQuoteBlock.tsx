cde8fef67a61c91048860e1fa7caf9dde68e6143
Md Alamin <alaminduc516@gmail.com>
2024-06-30 22:31:51 +0600

    :bug: eslint error fix

--- src/components/users/Home/TextQuoteBlock.tsx
+++ src/components/users/Home/TextQuoteBlock.tsx
@@ -21,9 +21,9 @@ const TextQuoteBlock = () => {
         <div>
         <p className="text-gray-800 text-xs lg:text-base font-medium mb-4">
           Mockplus DS is very well thought out and allows us to create designs
-          as quickly as possible with ease. We have several apps, that we've
+          as quickly as possible with ease. We have several apps, that we&apos;ve
           created mockups for, and we tried different tools, but currently we
-          moved entirely to Mockplus DS. I think it's way too underrated
+          moved entirely to Mockplus DS. I think it&apos;s way too underrated
           (especially in Europe) and provides a great value!
         </p>
         <div className="text-gray-600 font-semibold">Wladimir Alexi</div>
